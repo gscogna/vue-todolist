5b537043b1c0f957d2e4ed93ed3dd47cdea1586f
Gennaro <scognamiglioge@hotmail.it>
2021-01-31 12:09:51 +0100

    Problemi con array di oggetti.

--- js/script.js
+++ js/script.js
@@ -43,3 +43,49 @@ let app = new Vue({
         }
     }
 });
+
+
+
+// ew Vue({
+//     el: '#app',
+//     data: {
+//         todos: [
+//             { title: 'one value', edit: false },
+//             { title: 'one value', edit: false },
+//             { title: 'otro titulo', edit: false }
+//         ],
+//         editedTodo: null,
+//         message: 'Hello Vue.js!'
+//     },
+//     methods: {
+//         editTodo: function (todo) {
+//             this.editedTodo = todo
+//         }
+//     },
+//     directives: {
+//         focus: {
+//             inserted(el) {
+//                 el.focus()
+//             }
+//         }
+//     }
+// })
+// HTML
+
+//     < div id = "app" >
+//         Click the values to edit!
+//             < ul class="todo-list" >
+//                 <li v-for="todo in todos">
+//                     <input
+//                         v-if="todo.edit"
+//                         v-model="todo.title"
+//                 @blur="todo.edit = false; $emit('update')"
+//                 @keyup.enter="todo.edit=false; $emit('update')"
+//                 v-focus
+//             >
+//             <div v-else>
+//                         <label @{{ todo.title }} </label>
+//             </div>
+//         </li >
+//     </ >
+// </div >
